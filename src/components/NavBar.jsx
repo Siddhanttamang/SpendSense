b1243703b99f4cc8081b2c680ece7da73768c654
Siddhanttamang <tsiddhant2003@gmail.com>
2025-10-17 23:27:07 +0545

    setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar(){
+    return(
+            <ul className="flex flex-row gap-3">
+                <li> Home</li>
+                <li> About</li>
+                <li> Contact</li>
+            </ul>
+    )
+
+}
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div className="w-lvw bg-slate-400 h-lvh">
+            <p>This is home</p>
+            </div>
+    )
+
+}
+export default Home
